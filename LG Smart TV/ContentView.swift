b5570f3f4195474beebc8be7d0550debdecb4922
a13ks <a13ks@xxx.local>
2021-09-16 22:16:22 +0300

    added web socket wrapper class

--- LG Smart TV/ContentView.swift
+++ LG Smart TV/ContentView.swift
@@ -1,6 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    static let suiteName = "group.org.a13ks.lgtv"
+
+    @AppStorage("ipAddress", store: UserDefaults(suiteName: suiteName))
+    var ipAddress: String = ""
+    
+    @AppStorage("clientKey", store: UserDefaults(suiteName: suiteName))
+    var clientKey: String = ""
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
---
+++ LG Smart TV/WebSocketConnection.swift
@@ -0,0 +1,107 @@
+import Foundation
+import Combine
+import Network
+
+protocol WebSocketConnection {
+    func send(text: String)
+    func send(data: Data)
+    func connect()
+    func register()
+    func disconnect()
+    func openUrl(url: String)
+    var delegate: WebSocketConnectionDelegate? {
+        get
+        set
+    }
+}
+
+protocol WebSocketConnectionDelegate {
+    func onConnected(connection: WebSocketConnection)
+    func onDisconnected(connection: WebSocketConnection, error: Error?)
+    func onError(connection: WebSocketConnection, error: Error)
+    func onMessage(connection: WebSocketConnection, text: String)
+    func onMessage(connection: WebSocketConnection, data: Data)
+}
+
+class WebSocketTaskConnection: NSObject, WebSocketConnection, URLSessionWebSocketDelegate {
+    var delegate: WebSocketConnectionDelegate?
+    var webSocketTask: URLSessionWebSocketTask!
+    var urlSession: URLSession!
+    let delegateQueue = OperationQueue()
+    var connection: NWConnection?
+    
+    init(url: URL) {
+        super.init()
+        let configuration = URLSessionConfiguration.default
+        configuration.waitsForConnectivity = true
+        configuration.timeoutIntervalForRequest = 30
+        configuration.timeoutIntervalForResource = 300
+        
+        urlSession = URLSession(configuration: configuration, delegate: self, delegateQueue: delegateQueue)
+        webSocketTask = urlSession.webSocketTask(with: url)
+    }
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        self.delegate?.onConnected(connection: self)
+    }
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        self.delegate?.onDisconnected(connection: self, error: nil)
+    }
+    
+    func register() {
+        let hello = "{\"type\":\"register\",\"id\":\"register_0\",\"payload\":{\"forcePairing\":false,\"pairingType\":\"PROMPT\",\"manifest\":{\"manifestVersion\":1,\"appVersion\":\"1.1\",\"signed\":{\"created\":\"20140509\",\"appId\":\"com.lge.test\",\"vendorId\":\"com.lge\",\"localizedAppNames\":{\"\":\"LG Remote App\",\"ko-KR\":\"리모컨 앱\",\"zxx-XX\":\"ЛГ Rэмotэ AПП\"},\"localizedVendorNames\":{\"\":\"LG Electronics\"},\"permissions\":[\"TEST_SECURE\",\"CONTROL_INPUT_TEXT\",\"CONTROL_MOUSE_AND_KEYBOARD\",\"READ_INSTALLED_APPS\",\"READ_LGE_SDX\",\"READ_NOTIFICATIONS\",\"SEARCH\",\"WRITE_SETTINGS\",\"WRITE_NOTIFICATION_ALERT\",\"CONTROL_POWER\",\"READ_CURRENT_CHANNEL\",\"READ_RUNNING_APPS\",\"READ_UPDATE_INFO\",\"UPDATE_FROM_REMOTE_APP\",\"READ_LGE_TV_INPUT_EVENTS\",\"READ_TV_CURRENT_TIME\"],\"serial\":\"2f930e2d2cfe083771f68e4fe7bb07\"},\"permissions\":[\"LAUNCH\",\"LAUNCH_WEBAPP\",\"APP_TO_APP\",\"CLOSE\",\"TEST_OPEN\",\"TEST_PROTECTED\",\"CONTROL_AUDIO\",\"CONTROL_DISPLAY\",\"CONTROL_INPUT_JOYSTICK\",\"CONTROL_INPUT_MEDIA_RECORDING\",\"CONTROL_INPUT_MEDIA_PLAYBACK\",\"CONTROL_INPUT_TV\",\"CONTROL_POWER\",\"READ_APP_STATUS\",\"READ_CURRENT_CHANNEL\",\"READ_INPUT_DEVICE_LIST\",\"READ_NETWORK_STATE\",\"READ_RUNNING_APPS\",\"READ_TV_CHANNEL_LIST\",\"WRITE_NOTIFICATION_TOAST\",\"READ_POWER_STATE\",\"READ_COUNTRY_INFO\"],\"signatures\":[{\"signatureVersion\":1,\"signature\":\"eyJhbGdvcml0aG0iOiJSU0EtU0hBMjU2Iiwia2V5SWQiOiJ0ZXN0LXNpZ25pbmctY2VydCIsInNpZ25hdHVyZVZlcnNpb24iOjF9.hrVRgjCwXVvE2OOSpDZ58hR+59aFNwYDyjQgKk3auukd7pcegmE2CzPCa0bJ0ZsRAcKkCTJrWo5iDzNhMBWRyaMOv5zWSrthlf7G128qvIlpMT0YNY+n/FaOHE73uLrS/g7swl3/qH/BGFG2Hu4RlL48eb3lLKqTt2xKHdCs6Cd4RMfJPYnzgvI4BNrFUKsjkcu+WD4OO2A27Pq1n50cMchmcaXadJhGrOqH5YmHdOCj5NSHzJYrsW0HPlpuAx/ECMeIZYDh6RMqaFM2DXzdKX9NmmyqzJ3o/0lkk/N97gfVRLW5hA29yeAwaCViZNCP8iC9aO0q9fQojoa7NQnAtw==\"}]}}}";
+        
+        send(text: hello)
+    }
+    
+    func connect() {
+        webSocketTask.resume()
+        
+        listen()
+    }
+    
+    func disconnect() {
+        webSocketTask.cancel(with: .goingAway, reason: nil)
+    }
+    
+    func listen()  {
+        webSocketTask.receive { result in
+            switch result {
+            case .failure(let error):
+                self.delegate?.onError(connection: self, error: error)
+            case .success(let message):
+                switch message {
+                case .string(let text):
+                    self.delegate?.onMessage(connection: self, text: text)
+                case .data(let data):
+                    self.delegate?.onMessage(connection: self, data: data)
+                @unknown default:
+                    fatalError()
+                }
+                
+                self.listen()
+            }
+        }
+    }
+    
+    func send(text: String) {
+        webSocketTask.send(URLSessionWebSocketTask.Message.string(text)) { error in
+            if let error = error {                
+                self.delegate?.onError(connection: self, error: error)
+            }
+        }
+    }
+    
+    func send(data: Data) {
+        webSocketTask.send(URLSessionWebSocketTask.Message.data(data)) { error in
+            if let error = error {                
+                self.delegate?.onError(connection: self, error: error)
+            }
+        }
+    }
+
+    func openUrl(url: String) {
+        send(text: "{\"id\":\"21\",\"type\":\"request\",\"uri\":\"ssap://system.launcher/open\",\"payload\":{\"target\":\"http://www.inbox.lv\"}}")
+    }
+}
