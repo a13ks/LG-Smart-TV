539c02d016bae540914eba7383fa1b26662193bc
a13ks <a13ks@xxx.local>
2021-09-16 22:31:09 +0300

    ui work in progress

--- LG Smart TV/ContentView.swift
+++ LG Smart TV/ContentView.swift
@@ -9,9 +9,31 @@ struct ContentView: View {
     @AppStorage("clientKey", store: UserDefaults(suiteName: suiteName))
     var clientKey: String = ""
     
+    @State
+    var userInput: String = ""
+    
+    init() {
+        let userDefaults = UserDefaults(suiteName: ContentView.suiteName)
+        userInput = (userDefaults?.string(forKey: "ipAddress"))!
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if clientKey.isEmpty {
+            VStack {
+                Spacer().frame(height: 10)
+                Text("Enter IP address of TV").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Spacer().frame(height: 10)
+                TextField("IP address", text: $userInput)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                Spacer().frame(height: 10)
+                Button("Pair") {
+                    
+                }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            }
+        } else {
+            
+        }
     }
 }
 
